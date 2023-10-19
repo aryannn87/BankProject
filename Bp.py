dacdc7e7132d23842ec76da97d2bb76b07390afc
KalpeshRajput123 <145962356+KalpeshRajput123@users.noreply.github.com>
2023-10-19 14:26:32 +0530

    Update Bp.py

--- Bp.py
+++ Bp.py
@@ -1,124 +1,141 @@
-# Define the User class
 class User():
-    count = 0  # Initialize a class attribute for user count
-    
-    # Constructor to initialize user attributes
-    def __init__(self, name, gender, salary):
-        self.name = name
+    __count = 0
+    def __init__(self,name,gender,salary):
+        self.name = name                                  
         self.gender = gender
         self.salary = salary
-        User.count = User.count + 1  # Increment user count for each new user
-        self.account = User.count  # Assign an account number
-    
-    # Method to display user details
+        User.__count = User.__count+1
+        self.__account = User.__count
     def showdetails(self):
-        print(f"Name: {self.name}\nGender: {self.gender}\nSalary: {self.salary}")
-        print("Account No:", self.account)
-
-# Define the Bank class
+        print(f"Name :{self.name}\nGender : {self.gender}\nSalary : {self.salary}")
+        print("Account No :",self.__account)
 class Bank():
-    __balance = 0  # Initialize a private class attribute for bank balance
-    __bankname = "DNS Bank".center(30)  # Initialize a private class attribute for bank name
-    __usercount = 0  # Initialize a private class attribute for user count
-    
-    # Constructor to initialize bank account attributes
-    def __init__(self, name, gender, salary, pin):
+    __balance = 0
+    __bankname="DNS Bank".center(30)
+    __usercount = 0
+    def __init__(self,name,gender,salary,pin):
         self.name = name
-        self.gender = gender
+        self.gender  = gender
         self.salary = salary
-        self.__pin = pin  # Private PIN attribute
-        Bank.__usercount += 1  # Increment user count for each new bank account
-        self.account = f"bnkaccno000{Bank.__usercount}"  # Assign an account number
-    
-    # Method to deposit money
-    def deposit(self, amount):
+        self.__pin = pin
+        Bank.__usercount +=1
+        self.account = f"bnkaccno000{Bank.__usercount}"
+    def deposit(self,amount):
         self.__balance = self.__balance + amount
         print("Amount Deposited Successfully")
-        print("Your Current Balance is:", self.__balance)
-    
-    # Method to withdraw money
-    def withdraw(self, amount):
-        if amount > self.__balance:
+        print("Your Current Balance is :",self.__balance)
+    def withdraw(self,amount):
+        if amount>self.__balance:
             print("Insufficient Balance")
-            print("Your Current Balance is:", self.__balance)
-        elif amount >= 100 and amount <= self.__balance:
-            self.__balance = self.__balance - amount
+            print("Your Current Balance is :",self.__balance)
+        elif amount>=100 and amount<=self.__balance:
+            self.__balance = self.__balance-amount
             print("Thank You For Visiting")
-            print("Your Current Bank Amount is:", self.__balance)
-        elif amount < 100:
+            print("Your Current Bank Amount is :",self.__balance)
+        elif amount<100:
             print("You Cannot Withdraw Less Than 100")
-            print("Your Current Balance is:", self.__balance)
-    
-    # Method to view account balance
+            print("Your Current Balance is :",self.__balance)
     def viewbalance(self):
-        print(f"Name: {self.name}\nGender: {self.gender}\nSalary: {self.salary}\nCurrent Balance: {self.__balance}")
-    
-    # Method to transfer money to another account
-    def transfer(self, amt, user):
-        if amt > self.__balance:
+        print(f"Name : {self.name}\nGender : {self.gender}\nSalary : {self.salary}\nCurrent Balance :{self.__balance}")
+    def transfer(self,amt,user):
+        if amt>self.__balance:
             print("Insufficient Balance")
-            print("Your Current Balance is:", self.__balance)
-        elif amt >= 1 and amt <= self.__balance:
-            user.deposit(amt)  # Deposit amount into recipient's account
-            self.__balance = self.__balance - amt  # Subtract the transferred amount
+            print("Your Current Balance is :",self.__balance)
+        elif amt>=1 and amt<=self.__balance:
+            user.deposit(amt)
+            self.__balance = self.__balance-amt
             print("Amount Transfer Successfully")
-            print("Current Balance:", self.__balance)
-        elif amt < 1:
+            print("Current Balance :",self.__balance)
+        elif amt<1:
             print("You Cannot Transfer amount less than 1")
-            print("Current Balance:", self.__balance)
-    
-    # Method to get the username
+            print("Curent Balance :",self.__balance)
+            
     def getusername(self):
         return self.name
-    
-    # Method to get the PIN
     def getpin(self):
         return self.__pin
-    
-    # Method to return login data
     def logindata(self):
         return [self.name, self.__pin]
-    
-    # Method to provide a string representation of the object
     def __str__(self):
         return f"{self.name} {self.__pin}"
-
-# Create an empty dictionary to store user objects
+            
+              
 users = {}
 
-# Main program loop
 while True:
-    print("1. Create Account\n2. Login\n3. Exit")
-    choice = input("Enter your Selection: ")
-
+    print("1. Create Account\n2. Login\n->Exit")
+    choice = input("Enter your Selection : ")
+    
     if choice == "1":
-        # Create a new bank account and store it in the users dictionary
-        name = input("Enter your Name: ")
-        gender = input("Enter your Gender: ")
-        salary = int(input("Enter your Salary: "))
-        pin = input("Set your Password (PIN): ")
+        while True:
+            name = input("Enter your Name : ")
+            if name.isalpha():
+                break
+            else:
+                print("Invalid Name. Please enter only characters.")
+                
+        while True:
+            gender = input("Enter your Gender (1 for Male, 2 for Female, etc.): ")
+            if gender.isdigit():
+                break
+            else:
+                print("Invalid Gender. Please enter only Number.")
+
+        while True:
+            salary = input("Enter your Salary: ")
+            if salary.isdigit():
+                salary = int(salary)
+                break
+            else:
+                print("Invalid Salary. Please enter an Number.")
+
+        while True:
+            pin = (input("Set your Password (PIN) :"))
+            if pin.isdigit():
+                pin = int(pin)
+                break
+            else:
+                print("Invalid pin. Please enter an Number.")
+        
         users[name] = Bank(name, gender, salary, pin)
 
+
     elif choice == "2":
-        # Login to an existing account
-        name = input("Enter your Name: ")
-        pin = input("Enter your Password (PIN): ")
+        name = input("Enter your Name : ")
+        while True:
+            pin_input = input("Enter your Password (PIN): ")
+            if pin_input.isdigit():
+                pin = int(pin_input)
+                break
+            else:
+                print("Invalid PIN. Please enter an Number.")
 
         obj = users.get(name, 0)
 
         if obj == 0:
             print("No Match Found")
-        data = obj.logindata()
-        if [name, pin] in [data]:
+        elif obj.getpin() == pin:
             print("Access Granted")
             while True:
                 print("1. Deposit\n2. Withdraw\n3. View Balance\n4. Transfer\n5. Logout")
                 action = input("Choose an Action: ")
                 if action == "1":
-                    amount = int(input("Enter the deposit amount: "))
+                    while True:
+                        amount_input = input("Enter the deposit amount: ")
+                        if amount_input.isdigit():
+                            amount = int(amount_input)
+                            break
+                        else:
+                            print("Invalid amount. Please enter an Number.")
                     obj.deposit(amount)
                 elif action == "2":
-                    amount = int(input("Enter the withdraw amount: "))
+                    while True:
+                        amount_input = input("Enter the withdraw amount: ")
+                        if amount_input.isdigit():
+                            amount = int(amount_input)
+                            break
+                        else:
+                            print("Invalid amount. Please enter an Number.")
                     obj.withdraw(amount)
                 elif action == "3":
                     obj.viewbalance()
@@ -128,7 +145,13 @@ while True:
                     if account == 0:
                         print("Person Not Found")
                     else:
-                        amount = int(input("Enter the transfer amount: "))
+                        while True:
+                            amount = input("Enter the transfer amount: ")
+                            if amount.isdigit():
+                                amount = int(amount)
+                                break
+                            else:
+                                print("Invalid amount. Please enter an integer.")
                         obj.transfer(amount, account)
                 elif action == "5":
                     print("Logging Out")
@@ -137,8 +160,6 @@ while True:
                     print("Invalid Operation, Try again")
             else:
                 print("Access Denied")
-        else:
-            print("Access Denied")
 
     elif choice == "3":
         print("Exit")
